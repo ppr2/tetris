80db14154792144c4e08d53fd9bd0a05dec36fea
Matej Zidek <zidek.matej@gmail.com>
2015-11-02 19:34:44 +0100

    Merge branch 'master' of https://github.com/ppr2/tetris
    # Conflicts:
    #	.idea/workspace.xml

--- main.h
+++ main.h
@@ -10,9 +10,10 @@ typedef struct operation {
  * FUNCTION PROTOTYPES
  ************************************************/
 
-int branchFrom(State * state);
-int isLeaf(State state);
+void branchFrom(State * state);
+int isLeaf(State * state);
 void storeBestScore();
-void deleteLastOperation();
+void fit(Shape shapeToFit, char * map[], int index, Shape newValue);
+int fitable(Shape shape, char * map[], int index);
 
 #endif //PPR2_TETRIS_MAIN_H
